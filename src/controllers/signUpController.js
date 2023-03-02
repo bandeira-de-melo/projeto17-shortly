3a0f370d3be63eed8a593359e99bed3d9dd04d1e
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-03-02 18:51:00 -0300

    "refactor/finished signUp functionality"

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,16 +1,15 @@
 import { db } from "../db.connection.js"
 import bcrypt from 'bcrypt'
 
+
 export const signUpController = async (req, res)=>{
     const {name, email, password} = res.locals.data
     const cryptedPass =  bcrypt.hashSync(password, 10)
     try {
-        /* const emailExists =  await db.query(`SELECT email FROM users WHERE email = ${email}`)
-        console.log(emailExists) */
-        await db.query(`INSERT INTO "users" ("name", "email", "password") VALUES ($1, $2, $3)`, [name, email, password])
-        res.status(201).send("Done")
+        await db.query(`INSERT INTO "users" ("name", "email", "password") VALUES ($1, $2, $3)`, [name, email, cryptedPass])
+        res.status(201).send("user added successfully")
     } catch (error) {
-        res.send(error)
+        res.status(409).send(error.detail)
     }
     
 } 
